440a574cf3677e50a95fb343751c9527ca87a9e1
PasLoin <25326871+PasLoin@users.noreply.github.com>
2023-12-26 03:21:19 +0100

    Update trees_bxl_mobility_matching_from_csv.py

--- natural/trees_bxl_mobility_matching_from_csv.py
+++ natural/trees_bxl_mobility_matching_from_csv.py
@@ -2,38 +2,34 @@ import pandas as pd
 import osmium as o
 from geopy.distance import geodesic
 
-# Define a handler for processing OSM nodes
 class NodeCacheHandler(o.SimpleHandler):
     def __init__(self):
         super(NodeCacheHandler, self).__init__()
         self.node_cache = {}
 
     def node(self, n):
-        # Process each node in the OSM data
         if n.location.valid():
             node_location = (n.location.lat, n.location.lon)
-            # Store relevant information about the node in the cache
             self.node_cache[n.id] = {'node_id': n.id, 'location': node_location, 'tags': dict(n.tags)}
 
-# Define a class for matching trees between OSM and CSV data
 class OSMTreeMatcher:
-    def __init__(self, pbf_file_path, csv_file_path, output_csv_file_path, threshold_meters=2):
-        # Initialize the OSMTreeMatcher with file paths and a distance threshold
+    def __init__(self, pbf_file_path, csv_file_path, output_csv_file_path, output_osm_file_path, threshold_meters=2, max_tree_nodes=None):
         self.pbf_file_path = pbf_file_path
         self.csv_file_path = csv_file_path
         self.output_csv_file_path = output_csv_file_path
+        self.output_osm_file_path = output_osm_file_path
         self.threshold_meters = threshold_meters
+        self.max_tree_nodes = max_tree_nodes
         self.handler = NodeCacheHandler()
         self.csv_data = None
+        self.matched_data = None
 
     def read_osm_data(self):
-        # Read OSM data from the PBF file using the NodeCacheHandler
         osm_file = o.io.Reader(self.pbf_file_path)
         o.apply(osm_file, self.handler)
         osm_file.close()
 
     def read_csv_data(self):
-        # Read CSV data with specified column names and data types
         column_names = ['FID', 'gid', 'geom', 'numident', 'annee_plant', 'circumference', 'commune', 'couverture',
                         'crown_diam', 'essence', 'hauteur', 'multitronc', 'structure_couronne', 'status',
                         'espace_de_plantation', 'distribution', 'voirie']
@@ -41,41 +37,37 @@ class OSMTreeMatcher:
         self.csv_data = pd.read_csv(self.csv_file_path, names=column_names, skiprows=1, dtype=dtype_mapping, decimal=',')
 
     def extract_lat_lon(self, geom_str):
-        # Extract latitude and longitude from the 'geom' column in the CSV data
         lon, lat = map(float, geom_str.split('(')[-1].split(')')[0].split())
         return lat, lon
 
     def search_pbf_nodes(self):
-        # Search for nodes in the PBF file with 'natural=tree' and 'circumference=0'
         tree_nodes = []
 
         for node_id, node_info in self.handler.node_cache.items():
             tags = node_info['tags']
             circumference = tags.get('circumference')
 
-            # Check if the node has 'natural=tree' and 'circumference=0'
-            if tags.get('natural') == 'tree' and circumference is not None and circumference == '0':
-                # Collect additional tags for the tree node
+            if tags.get('natural') == 'tree' and circumference is not None and float(circumference) > 100:
                 additional_tags = {key: value for key, value in tags.items() if key != 'natural' and key != 'circumference'}
-
                 tree_nodes.append({
                     'node_id': node_id,
                     'location': node_info['location'],
-                    'circumference': circumference,
                     'additional_tags': additional_tags
                 })
 
-        # Print the count of tree nodes found
-        print(f"Number of tree nodes found in PBF: {len(tree_nodes)}")
+                if self.max_tree_nodes is not None and len(tree_nodes) >= self.max_tree_nodes:
+                    break
 
+        print(f"Number of tree nodes found in PBF: {len(tree_nodes)}")
         return tree_nodes
 
     def match_trees(self, tree_nodes):
-        # Create a list to store the matching data
         matched_data_list = []
 
-        # Match trees based on coordinates between CSV and PBF data
         for _, csv_row in self.csv_data.iterrows():
+            if csv_row['status'] != 'en vie' or csv_row['circumference'] == '0':
+                continue
+
             lat, lon = self.extract_lat_lon(csv_row['geom'])
 
             for node_info in tree_nodes:
@@ -83,35 +75,66 @@ class OSMTreeMatcher:
                 distance = geodesic((lat, lon), (node_lat, node_lon)).meters
 
                 if distance < self.threshold_meters:
-                    # Append matching data to the list
                     matched_data_list.append({
                         'Node_ID': node_info['node_id'],
                         'CSV_Row_Index': _,
                         'Distance': distance,
                         'Numident': csv_row['numident'],
-                        'Circumference': node_info['circumference']
+                        'Circumference': csv_row['circumference']
                     })
 
-                    # Print matching data during the process
                     print(f"Match found for CSV row {_} with Node ID {node_info['node_id']} "
                           f"(Distance: {distance:.2f} meters), "
-                          f"Numident: {csv_row['numident']}, Circumference: {node_info['circumference']}")
+                          f"Numident: {csv_row['numident']}, Circumference (CSV): {csv_row['circumference']}")
 
-        # Create a DataFrame from the list of dictionaries
-        matched_data = pd.DataFrame(matched_data_list)
-
-        # Save the matching data to a new CSV file
-        matched_data.to_csv(self.output_csv_file_path, index=False)
+        self.matched_data = pd.DataFrame(matched_data_list)
+        self.matched_data.to_csv(self.output_csv_file_path, index=False)
         print(f"Matching data saved to {self.output_csv_file_path}")
+
+    def generate_osm_file(self, tree_nodes):
+        with open(self.output_osm_file_path, 'w', encoding='utf-8') as osm_file:
+            osm_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            osm_file.write('<osm version="0.6" generator="osmium/1.14">\n')
+
+            # Write nodes from matching CSV data
+            for _, row in self.matched_data.iterrows():
+                node_id = row['Node_ID']
+                lat, lon = self.extract_lat_lon(self.csv_data.loc[row['CSV_Row_Index'], 'geom'])
+                circumference_m = float(row['Circumference']) / 100.0
+
+                osm_file.write(f'  <node id="{node_id}" lat="{lat}" lon="{lon}" version="1">\n')
+                osm_file.write('    <tag k="natural" v="tree" />\n')
+                osm_file.write(f'    <tag k="circumference" v="{circumference_m}" />\n')
+                osm_file.write(f'    <tag k="height" v="{self.csv_data.loc[row["CSV_Row_Index"], "hauteur"]}" />\n')
+                osm_file.write(f'    <tag k="species" v="{self.csv_data.loc[row["CSV_Row_Index"], "essence"]}" />\n')
+                osm_file.write('  </node>\n')
+
+            osm_file.write('</osm>\n')
+            print(f"OSM file generated and saved to {self.output_osm_file_path}")
 
 if __name__ == "__main__":
     # Specify the paths to the PBF and CSV files
     pbf_path = 'brussels_capital_region.pbf'
     csv_path = 'trees.csv'
-    output_csv_path = 'matched_data.csv'  # Specify the desired output CSV file path
+    output_csv_path = 'matched_data.csv'
+    output_osm_path = 'matched_data.osm'
 
-    # Create an instance of OSMTreeMatcher with the output CSV file path
-    tree_matcher = OSMTreeMatcher(pbf_path, csv_path, output_csv_path)
+    # Prompt the user for the maximum number of tree nodes
+    max_tree_nodes = input("Enter the maximum number of tree nodes to process (or press Enter for no limit): ")
+    if max_tree_nodes.strip():
+        max_tree_nodes = int(max_tree_nodes)
+    else:
+        max_tree_nodes = None
+
+    # Prompt the user for the threshold distance
+    threshold_meters = input("Enter the threshold distance in meters (press Enter for the default value of 0.2): ")
+    if threshold_meters.strip():
+        threshold_meters = float(threshold_meters)
+    else:
+        threshold_meters = 0.2
+
+    # Create an instance of OSMTreeMatcher with the output CSV and OSM file paths
+    tree_matcher = OSMTreeMatcher(pbf_path, csv_path, output_csv_path, output_osm_path, threshold_meters, max_tree_nodes=max_tree_nodes)
 
     # Read OSM data
     tree_matcher.read_osm_data()
@@ -124,3 +147,6 @@ if __name__ == "__main__":
 
     # Match trees between CSV and PBF data
     tree_matcher.match_trees(tree_nodes)
+
+    # Generate OSM file
+    tree_matcher.generate_osm_file(tree_nodes)
